cf8a0a66a4a774d0d78665bea89876a093dd41d0
xbendorx <omri.bendor@gmail.com>
2016-08-27 21:12:43 +0200

    Design

--- index.php
+++ index.php
@@ -1,8 +1,59 @@
 <html>
 <head>
 	<title>BENDOR</title>
+	<style type="text/css">
+	body {
+		font-family: Georgia;
+		background-color: #272822;
+		color: #F92672;
+	}
+	#main {
+		font-size: 100px;
+		/*padding-top: 10%;*/
+		text-shadow: 2px 2px 34px #66D9EF;
+	}
+	#smiley {
+		width: 150px;
+		height: 150px;
+		-webkit-filter: drop-shadow(0px 0px 18px #FD971F);
+	}
+	#cN {
+		width: 150px;
+		height: 150px;
+		-webkit-filter: drop-shadow(0px 0px 15px white);
+	}
+	hr {
+		border-color: #A6E22E;
+    	border-style: solid;
+    	border-width: 2px;
+    	/*box-shadow: 0px 0px 5px 0px #A6E22E;*/
+	}
+	</style>
 </head>
 <body>
-XXX
+	<center>
+	<br><br><br><!-- <br><br> -->
+	<BR><BR>
+	<img id="cN" src="cN.png">
+	<BR><BR>
+	<hr>
+	<div id="main">
+		BENDOR
+	</div>	
+	<hr>
+	<br><br>	
+	<img id="smiley" src="awesomeSmiley.png">
+	</center>
 </body>
 </html>
+<script type="text/javascript">
+
+// var colors_grey   = "#272822";
+// var colors_pink   = "#F92672";
+// var colors_cyan   = "#66D9EF";
+// var colors_green  = "#A6E22E";
+// var colors_orange = "#FD971F";
+
+
+</script>
+
