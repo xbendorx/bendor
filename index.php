921c464ce3fc209937e9111c590264c320cd0fa5
xbendorx <omri.bendor@gmail.com>
2016-08-28 14:09:33 +0200

    Click logic

--- index.php
+++ index.php
@@ -9,17 +9,13 @@
 	}
 	#main {
 		font-size: 100px;
-		/*padding-top: 10%;*/
 		text-shadow: 2px 2px 34px #66D9EF;
 	}
-	#smiley {
+	#logo {
 		width: 150px;
 		height: 150px;
-		-webkit-filter: drop-shadow(0px 0px 18px #FD971F);
 	}
-	#cN {
-		width: 150px;
-		height: 150px;
+	.WhiteShadowed {
 		-webkit-filter: drop-shadow(0px 0px 15px white);
 	}
 	hr {
@@ -33,29 +29,34 @@
 </head>
 <body>
 	<center>
-	<br><br><br><!-- <br><br> -->
-	<BR><BR>
-	<img id="cN" src="cN.png">
+	<br><br><br><BR><BR>
+	<img id="logo" src="cN.png" class="WhiteShadowed">
 	<BR><BR><br>
-	<!-- <hr> -->
 	<div id="main">
 		BENDOR
-	</div>	
+	</div>
 	<br><br><br>
 	<hr>
-	<br><br>	
-	<!-- <img id="smiley" src="awesomeSmiley.png"> -->
+	<br><br>
 	</center>
 </body>
 </html>
 <script type="text/javascript">
 
-// var colors_grey   = "#272822";
-// var colors_pink   = "#F92672";
-// var colors_cyan   = "#66D9EF";
-// var colors_green  = "#A6E22E";
-// var colors_orange = "#FD971F";
 
+logo.onclick = function() {
+	if(logo.src.endsWith("cn.png")) {
+		logo.src = "awesomeSmiley.png";
+	} else {
+		logo.src = "cn.png";
+	}
+}
+
+// var monokai_color_grey   = "#272822";
+// var monokai_color_pink   = "#F92672";
+// var monokai_color_cyan   = "#66D9EF";
+// var monokai_color_green  = "#A6E22E";
+// var monokai_color_orange = "#FD971F";
 
 </script>
 
